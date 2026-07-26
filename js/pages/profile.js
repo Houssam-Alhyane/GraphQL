09415eb1f556117bbed19b866a5271cb02541e75
halhyane <houssamalhyan5@gmail.com>
2026-07-26 13:27:57 +0100

    fix KB and MB

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -2,6 +2,7 @@ import { getUser } from '../services/graphql.js';
 import { getToken, removeToken } from '../utils/storage.js';
 import { renderAuditGraph } from '../graphs/auditGraph.js';
 import { renderResultGraph } from '../graphs/ResultGraph.js';
+import { formatXP } from '../utils/helpers.js';
 
 document.getElementById('logoutBtn').addEventListener('click', () => {
   removeToken();
@@ -21,8 +22,7 @@ if (!token) {
     document.getElementById('auditRatio').textContent =
       user.auditRatio.toFixed(2);
     document.getElementById('userLevel').textContent = user.events[0].level;
-    document.getElementById('xpTotal').textContent =
-      user.totalXP.toLocaleString().split(',')[0] + ' KB';
+    document.getElementById('xpTotal').textContent = formatXP(user.totalXP);
   }
   renderResultGraph(user.passed, user.failed);
   renderAuditGraph(user.audits);
