69d1af8097454439faad6fedfc79a0741a6e058d
halhyane <houssamalhyan5@gmail.com>
2026-07-27 10:08:07 +0100

    refactor query

--- js/services/query.js
+++ js/services/query.js
@@ -1,50 +1,65 @@
-export const query = `{
-  user {
-    avatarUrl
-    id
-    login
-    auditRatio
+const userQuery = `
+user {
+  avatarUrl
+  id
+  login
+  auditRatio
 
-    events(
-      where: {
-        eventId: { _eq: 41 }
-      }
-    ) {
-      level
-    }
-
-    audits(
-      where: {
-        closureType: {
-          _in: [succeeded, failed, expired]
-        }
-      }
-    ) {
-      closureType
-    }
-  }
-
-  totalXP: transaction_aggregate(
-    where: {
-      type: { _eq: "xp" }
-      event: {
-        object: {
-          name: { _eq: "Module" }
-        }
-      }
-    }
-  ) {
-    aggregate {
-      sum {
-        amount
-      }
-    }
-  }
-  results: result(
+  events(
     where: {
       eventId: { _eq: 41 }
     }
   ) {
-    grade
+    level
   }
-}`;
+
+  audits(
+    where: {
+      closureType: {
+        _in: [succeeded, failed, expired]
+      }
+    }
+  ) {
+    closureType
+  }
+}
+`;
+
+const xpQuery = `
+totalXP: transaction_aggregate(
+  where: {
+    type: { _eq: "xp" }
+    event: {
+      object: {
+        name: { _eq: "Module" }
+      }
+    }
+  }
+) {
+  aggregate {
+    sum {
+      amount
+    }
+  }
+}
+`;
+
+const resultQuery = `
+results: result(
+  where: {
+    eventId: { _eq: 41 }
+  }
+) {
+  grade
+}
+`;
+
+export const query = `
+{
+  ${userQuery}
+
+  ${xpQuery}
+
+  ${resultQuery}
+}
+`;
