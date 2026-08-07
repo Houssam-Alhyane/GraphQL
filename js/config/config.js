a303a9fe9c56fee10a645127658ac9d08b91bc7e
halhyane <houssamalhyan5@gmail.com>
2026-08-07 17:47:35 +0100

    rm

--- js/config/config.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_URL = 'https://learn.zone01oujda.ma/api/auth/signin';
-
-export const GRAPHQL_URL =
-  'https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql';
--- js/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export function formatXP(bytes) {
-  const kb = bytes / 1000;
-  if (kb >= 1000) {
-    return (kb / 1000).toFixed(2) + ' MB';
-  }
-  return Math.round(kb).toLocaleString() + ' KB';
-}
